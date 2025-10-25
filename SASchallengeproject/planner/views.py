ad017f151f2594da629ac679adc43e0b720ce00d
salwa sayed <2895585s@student.gla.ac.uk>
2025-10-25 14:41:19 +0100

    Base 'views.py' file

--- SASchallengeproject/planner/views.py
+++ SASchallengeproject/planner/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# HOME PAGE
+# DESCRIPTION: Select filters for event ideas
+def home(request):
+    return render(request, 'planner/home.html')
+
+# SLOT MACHINE PAGE
+# DESCRIPTION: Randomly selects event ideas from given filters
+def slot(request):
+    return render(request, 'planner/slot.html')
