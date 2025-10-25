0b41f9b92811d65d75664f62e5d681702c767480
salwa sayed <2895585s@student.gla.ac.uk>
2025-10-25 15:53:33 +0100

    Views and URLs Connected

--- SASchallengeproject/planner/views.py
+++ SASchallengeproject/planner/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # HOME PAGE
 # DESCRIPTION: Select filters for event ideas
+#def home(request):
+#    context_dict = {}
+#
+#    #response = render(request, 'planner/home.html', context_dict)
+#    return request
+
 def home(request):
-    return render(request, 'planner/home.html')
+    return HttpResponse("Rango says hey there partner!")
+
 
 # SLOT MACHINE PAGE
 # DESCRIPTION: Randomly selects event ideas from given filters
-def slot(request):
-    return render(request, 'planner/slot.html')
+#def slotmachine(request):
+#    context_dict = {}
+#
+#    #response = render(request, 'planner/slotmachine.html', context_dict)
+#    return request
+
+def slotmachine(request):
+    return HttpResponse("Rango says hey there partner!")
